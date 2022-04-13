cbebb483a57c412f5067ad8632d7d644f1aa28e5
Senichkaa <super-kapanaiko406@ukr.net>
2022-04-13 15:51:41 +0300

    wokr on web site

---
+++ Areas/Identity/Data/ApplicationUser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace ExDrive_web.Areas.Identity.Data;
+
+// Add profile data for application users by adding properties to the ApplicationUser class
+public class ApplicationUser : IdentityUser
+{
+    [Key]
+    public int ID { get; set; }
+    [Required]
+    public string Login { get; set; }
+}
+
